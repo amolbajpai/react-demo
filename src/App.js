b802e8dbddad24dbb0d9b932e41b8c42870a061b
Amol Bajpai <amolbajpai20@gmail.com>
2024-12-20 01:07:53 +0530

    Create Footer section component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
 
   return (
     <div className="main">
-      <Header/>
+      <Header />
+      <Footer />
 
     </div>
   );
