2a4435cac2b5229156b82bdca3be5eb4aa34a7aa
Amol Bajpai <amolbajpai20@gmail.com>
2024-12-20 01:04:14 +0530

    Created Header component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
-  let names = ["Ram", "Krishna", "Shyam"]
-  let obj = {
-    "name": "Amol",
-    "address" : "Noida"
-  }
-  let status = true;
+
   return (
     <div className="main">
-      <h1>Jai Shri Ram</h1>
-      {names.map((v) => {
-        return (
-          <div><h1>{v}</h1></div>
-        )
-      })}
-      <h2>{obj.name}</h2>
-      {(status) ?
-        <h1 style={{color:"red",backgroundColor: "yellow"}}>This is my App</h1>
-        :
-        <h1>Status is false</h1>
-      }
+      <Header/>
+
     </div>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+      <div>
+            <h1>Welcome to Header</h1>
+            <h2>It's My app</h2>
+        </div>
+  )
+}
