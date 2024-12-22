34c4f9ccb1905cd31ef9f906b0de924ba5091cb2
Amol Bajpai <amolbajpai20@gmail.com>
2024-12-22 17:49:06 +0530

    Example of adding images in project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header';
 import Footer from './Footer';
 import { blog } from './Data/blog';
+import savarkar from './images/Veer-Savarkar.jpg'
 
 function App() {
   let name = { "name": "Amol Bajpai" , "add" : "Pune" }
@@ -11,6 +12,7 @@ function App() {
     <div className="main">
       
       <Header info={name}>Shri Krishnay Namah</Header>
+      <img src={savarkar} alt='Veer Savarkar' width="200" height="200"/>
       <div className='row'>
         {blog.map((value,i) => {
           return (<Card pitem = {value} key={i} />)
