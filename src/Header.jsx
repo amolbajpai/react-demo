f8e5cb944d32b428888db5b071f1576a44c8712d
Amol Bajpai <amolbajpai20@gmail.com>
2024-12-22 17:39:53 +0530

    Example of adding css file in project

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Header.css'
 
 export default function Header({ info,children }) {
   console.log(children)
     return (
-      <div>
-            <h1>Welcome to Header</h1>
+      <div className='header'>
+        <h1>Welcome to Header</h1>
         <h2>It's My app</h2>
         <h3>{info.name}</h3>
         <h3>{info.add}</h3>
